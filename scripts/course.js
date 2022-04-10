45a7cdc5b79c4752ca5b262aa62a93d6436ad3a7
MaximTutunov <97736607+MaximTutunov@users.noreply.github.com>
2022-04-10 19:11:29 +0300

    FAQ BEM FIX

--- scripts/course.js
+++ scripts/course.js
@@ -21,16 +21,17 @@ cardList.forEach((item) => {
 });
 
 //function FAQ
-const btnList = document.querySelectorAll(".card__faq-container");
+
+const btnList = document.querySelectorAll(".faq");
 
 btnList.forEach((item) => {
-  const buttonWrap1 = item.querySelector(".card__button");
-  const contentWrap = item.querySelector(".card__content");
-  const cardHeaderWrap = item.querySelector(".card__header_type_faq");
+  const buttonWrap1 = item.querySelector(".faq__button");
+  const contentWrap1 = item.querySelector(".faq__content");
+  const cardHeaderWrap = item.querySelector(".faq__header");
   cardHeaderWrap.addEventListener("click", function (evt) {
     evt.stopPropagation();
 
-    contentWrap.classList.toggle("card__content_open");
-    buttonWrap1.classList.toggle("card__button_up");
+    contentWrap1.classList.toggle("faq__content_open");
+    buttonWrap1.classList.toggle("faq__button_up");
   });
 });
