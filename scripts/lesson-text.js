d76fa5a898e0d08700154435f1e28a54adf4b6c5
SmokyDogg <golovkin47@gmail.comgit config --global user.email golovkin47@gmail.com>
2022-04-10 17:15:57 +0300

    Теперь все стрелочки крутятся на 2 и 3 странице

--- scripts/lesson-text.js
+++ scripts/lesson-text.js
@@ -16,9 +16,11 @@ const sideBarList = document.querySelectorAll(".lesson-side");
 sideBarList.forEach((item) => {
   const buttonWrap = item.querySelector(".lesson-side__header");
   const contentWrap = item.querySelector(".lesson-side__container");
+  const lessonSideBtn = item.querySelector('.lesson-side__button');
   buttonWrap.addEventListener("click", function (evt) {
     evt.stopPropagation();
 
     contentWrap.classList.toggle("lesson-side__container_closed");
+    lessonSideBtn.classList.toggle('lesson-side__button_active');
   });
 });
